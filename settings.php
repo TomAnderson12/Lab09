33df1a772c8e904c0a3ab7622330ec291862e90f
TomAnderson12 <105916403@student.swin.edu.au>
2025-05-14 12:48:59 +1000

    Connected DB

---
+++ settings.php
@@ -0,0 +1,5 @@
+<?php
+$host = "localhost";         // because XAMPP runs the server locally
+$user = "root";          // default username for XAMPP's MySQL
+$pwd = "";              // default password is empty in XAMPP
+$sql_db = "exhibition_db";  // replace with the actual name of your database
